db2f34a1a467099327459fc2f7bd178d429ca5ce
blackwellsmith <blackwellsmithiv@gmail.com>
2020-06-10 14:05:03 -0400

    moved facebook link to the right the footer

--- src/footer.js
+++ src/footer.js
@@ -5,7 +5,7 @@ import './App.css';
 const Footer = () => {
     return (
         <header className="footer">
-            <h1><a href="https://www.facebook.com/The-Official-Richard-Lloyd-Fan-Page-126158157415048/"><img src={facebook} height="50" width="50" /></a></h1>
+            <h3>Fan Page on <a href="https://www.facebook.com/The-Official-Richard-Lloyd-Fan-Page-126158157415048/"><img src={facebook} height="50" width="50" /></a></h3>
        </header> 
     )
 }
